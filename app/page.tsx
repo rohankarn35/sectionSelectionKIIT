b7b4d9457e8785943a4fa9a9392e9fab16300255
technicalranjitofficial <dranjitkumar16@gmail.com>
2023-07-30 20:37:41 +0530

    all bugs fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
-
 "use client"
 import { signIn } from "next-auth/react";
 
-export default  function page() {
-  return(
-    <div className="w-full h-full min-h-screen flex flex-col items-center justify-center">
-<h1>You are Not Logged In Please login to continue </h1>
-<button className="bg-green-700 py-2 px-10 mt-10 rounded-md" onClick={()=>signIn("google")}>Login</button>
+export default function Page() {
+  const handleSignIn = () => {
+    signIn("google");
+  };
+
+  return (
+    <div className="w-full h-full min-h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-gray-900 p-8 rounded-lg shadow-md max-w-md w-full">
+        <h1 className="text-xl md:text-2xl  font-bold text-center mb-6">
+          You are Not Logged In. Please login to continue.
+        </h1>
+        <button
+          className="bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600 py-3 px-6 rounded-md text-white w-full"
+          onClick={handleSignIn}
+        >
+          Login with Google
+        </button>
+      </div>
     </div>
-  )
+  );
 }
